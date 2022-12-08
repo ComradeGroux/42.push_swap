a45dbb2d82568f74400baa6cc5cb1336a0df7ae4
Victor Groux <victorgroux99@gmail.com>
2022-12-08 03:14:04 +0100

    debut gestion inputs

--- srcs/error.c
+++ srcs/error.c
@@ -6,15 +6,30 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:36:41 by vgroux            #+#    #+#             */
-/*   Updated: 2022/12/07 17:40:17 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/12/08 00:20:44 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_bool	check_isexist(void)
+t_bool	check_isnotdouble(char *argv)
 {
-	return (FALSE);
+	int	i;
+	int	j;
+
+	i = 0;
+	while (argv[i])
+	{
+		j = i + 1;
+		while (argv[j])
+		{
+			if (argv[i] == argv[j])
+				return (FALSE);
+			j++;
+		}
+		i++;
+	}
+	return (TRUE);
 }
 
 t_bool	check_onlydigit(char *arg)
@@ -33,7 +48,25 @@ t_bool	check_onlydigit(char *arg)
 
 t_bool	check_arg(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		if (check_onlydigit(argv[1]))
+			return (check_isexist(argv[1]));
+		else
+			return (FALSE);
+	}
+	else
+	{
+		while (++i < argc)
+		{
+			if (!check_onlydigit(argv[i]))
+				return (FALSE);
+			i++;
+		}
+		
+	}
 	return (FALSE);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:16:22 by vgroux            #+#    #+#             */
-/*   Updated: 2022/12/07 17:38:09 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/12/08 00:24:21 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ int	main(int argc, char **argv)
 	// t_list	**a;
 	// t_list	**b;
 
-	(void)argc;
-	(void)argv;
-	ft_printf("%d\n", check_onlydigit(argv[1]));
+	if (check_arg(argc, argv) == FALSE)
+		ft_putendl_fd("Error", 2);
+	else
+	{
+		
+	}
 	return (0);
 }
