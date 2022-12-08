32e5d7fc9088b01a02b60a5cc9e456f0b649ae3a
vgroux <vgroux@student.42lausanne.ch>
2022-12-08 16:20:08 +0100

    .

--- srcs/error.c
+++ srcs/error.c
@@ -6,24 +6,24 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:36:41 by vgroux            #+#    #+#             */
-/*   Updated: 2022/12/08 00:20:44 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/12/08 14:42:46 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_bool	check_isnotdouble(char *argv)
+t_bool	check_isnotdouble(int arr[], int len)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (argv[i])
+	while (i < len)
 	{
 		j = i + 1;
-		while (argv[j])
+		while (j < len)
 		{
-			if (argv[i] == argv[j])
+			if (arr[i] == arr[j])
 				return (FALSE);
 			j++;
 		}
@@ -49,24 +49,19 @@ t_bool	check_onlydigit(char *arg)
 t_bool	check_arg(int argc, char **argv)
 {
 	int	i;
+	int	*arr;
 
 	i = 0;
 	if (argc == 2)
-	{
-		if (check_onlydigit(argv[1]))
-			return (check_isexist(argv[1]));
-		else
+		if (!check_onlydigit(argv[1]))
 			return (FALSE);
-	}
 	else
 	{
 		while (++i < argc)
 		{
 			if (!check_onlydigit(argv[i]))
 				return (FALSE);
-			i++;
 		}
-		
 	}
 	return (FALSE);
 }
